ab132d246320e56013b37ddede33b219d911d6dd
Hub-qianyue <903978593@qq.com>
2025-03-11 16:04:10 +0800

    test1

--- testcases/test_open.py
+++ testcases/test_open.py
@@ -3,5 +3,8 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach', True)
 #创建浏览器对象
-driver = webdriver.Chrome(options=options)
-driver.get('https://www.baidu.com')
+
+class TestOpen:
+    def test_open(self):
+        driver = webdriver.Chrome(options=options)
+        driver.get('https://www.baidu.com')
